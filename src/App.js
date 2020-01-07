d64d15e659f1aa5d7c1d84c3aee70f3e2edf6b17
BG4 <ravi.p.kasturi@gmail.com>
2020-01-07 21:38:28 +0530

    code cleaned

--- src/App.js
+++ src/App.js
@@ -7,27 +7,7 @@ import Cart from "./pages/Cart"
 import Photos from "./pages/Photos"
 
 function App() {
-    const [count, setCount] = useState(0)
-    const [color, setColor] = useState("red")
-    
-    function increment() {
-        setCount(prevCount => prevCount + 1)
-    }
-    
-    function decrement() {
-       if(count>0){
-            setCount((prevCount) => {
-              return prevCount - 1
-            })
-          }
-    }
-    
-    useEffect(() => {
-      if (count > 0 ){
-        setColor(randomcolor())
-    }else{setColor("red")} }, [count])
-    
-    
+
     
     return (
         <div>
@@ -35,18 +15,7 @@ function App() {
             <Switch>
               <Route exact path="/"><Photos /> </Route>
               <Route path="/cart"><Cart /> </Route>            
-            </Switch>
-            {/*
-            <h1 style={{color: color}}>{count}</h1>
-            <button onClick={increment}>Increment</button>
-            <button onClick={decrement}>Decrement</button>
-
-          
-            <div class="aplphaGrid">
-              <span class="alphabet_box" style={{backgroundColor:randomcolor()}}>A</span>
-              <span class="alphabet_box" style={{backgroundColor:randomcolor()}}>B</span>
-            </div>
-            */}
+            </Switch>}
         </div>
         
     )
